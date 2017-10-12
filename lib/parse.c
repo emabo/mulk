e04910ad8b1ed62796d5c3a4131f692b08814298
Emanuele Bovisio <3024205+emabo@users.noreply.github.com>
2017-10-12 22:28:41 +0200

    remove annoying compilation warning when including tidy headers

--- lib/parse.c
+++ lib/parse.c
@@ -35,10 +35,10 @@
 #ifdef ENABLE_RECURSION
 #if defined(HAVE_TIDY_H) || defined(_WIN32)
 #include <tidy.h>
-#include <buffio.h>
+#include <tidybuffio.h>
 #elif defined(HAVE_TIDY_TIDY_H)
 #include <tidy/tidy.h>
-#include <tidy/buffio.h>
+#include <tidy/tidybuffio.h>
 #else
 #error "Don't know where to look for libtidy headers"
 #endif
